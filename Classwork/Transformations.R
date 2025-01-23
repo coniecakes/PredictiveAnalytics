b9a06b629d35ab3c4889e5beb6cdb2e94c2f4278
Conie O'Malley <co1984a@american.edu>
2025-01-22 19:31:58 -0500

    R Exercise 4

--- Classwork/Transformations.R
+++ Classwork/Transformations.R
@@ -6,3 +6,13 @@ summary(lm((log(price)) ~ carat, data = diamonds))
 summary(lm(price ~ log(carat), data = diamonds))
 
 summary(lm(log(price) ~ log(carat), data = diamonds))
+
+# centering
+library(MASS)
+Boston.centered <- data.frame(scale(Boston, center = TRUE, scale = FALSE))
+summary(lm(medv ~ lstat + age, data = Boston))
+summary(lm(medv ~ lstat + age - 1, data = Boston.centered))
+plot(lm(medv ~ lstat + age - 1, data = Boston.centered))
+plot(lm(medv ~ lstat + age, data = Boston))
+
+# Standardization using z-score
